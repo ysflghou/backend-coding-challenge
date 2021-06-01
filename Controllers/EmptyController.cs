08bc7568c921e9f7cfaa7ced129617d381540d6a
youssef lghoumaigui <yousseflghoumaigui@gmail.com>
2021-06-01 01:05:17 +0100

    Initial commit

---
+++ Controllers/EmptyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace backend_coding_challenge.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmptyController : ControllerBase
+    {
+        [HttpGet]
+        public EmptyModel Get()
+        {
+            return new EmptyModel();
+        }
+    }
+}
---
+++ Models/EmptyModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace backend_coding_challenge
+{
+    public class EmptyModel
+    {
+        public string Hello => "Hello from an empty model !";
+    }
+}
