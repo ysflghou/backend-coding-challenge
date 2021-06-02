be57f372d051cc783ebe6f6e6b0d7a7507310397
youssef lghoumaigui <yousseflghoumaigui@gmail.com>
2021-06-03 00:19:31 +0100

    Remove unused using directives

--- GithubClient/ITrendingRepositoriesClient.cs
+++ GithubClient/ITrendingRepositoriesClient.cs
@@ -1,7 +1,4 @@
 ﻿using backend_coding_challenge.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace backend_coding_challenge.GithubClient
