e66521410b14e8ab2c1bd2f08aa200339d2f745a
youssef lghoumaigui <yousseflghoumaigui@gmail.com>
2021-06-03 00:24:11 +0100

    Introduce ProgrammingLanguages model

---
+++ Models/ProgrammingLanguages.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace backend_coding_challenge.Models
+{
+    [DataContract]
+    public class ProgrammingLanguages
+    {
+        [DataMember]
+        [JsonPropertyName("total_count")]
+        public int TotalCount { get; set; }
+
+        [DataMember]
+        [JsonPropertyName("languages")]
+        public IReadOnlyList<ProgrammingLanguage> Languages { get; set; }
+    }
+}
