36973af5b7b6a9bc51337043d474f8279a506783
youssef lghoumaigui <yousseflghoumaigui@gmail.com>
2021-06-02 23:01:53 +0100

    Add ProgrammingLanguage model
    This is the model that will be used in the result of our API

---
+++ Models/ProgrammingLanguage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend_coding_challenge.Models
+{
+    public class ProgrammingLanguage
+    {
+        public string Name { get; set; }
+        public int NumberOfGithubRepositories { get; set; }
+        public IReadOnlyList<GithubRepository> GithubRepositories;
+    }
+}
