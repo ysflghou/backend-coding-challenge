e6ef9a9e5c87be84f60c5d7120c2c126a3bafb77
youssef lghoumaigui <yousseflghoumaigui@gmail.com>
2021-06-05 03:35:42 +0100

    Add RequestUriBuilderTests

---
+++ backend-coding-challenge.Tests/GithubClient/RequestUriBuilderTests.cs
@@ -0,0 +1,19 @@
+﻿using backend_coding_challenge.GithubClient;
+using NUnit.Framework;
+using System;
+
+namespace backend_coding_challenge.Tests.GithubClient
+{
+    internal class RequestUriBuilderTests
+    {
+        [Test]
+        public void BuildRequestUri_Returns_CorrectGithubTrendingRepositoriesUri()
+        {
+            var todaysDate = DateTime.Today;
+            var monthBeforeToday = todaysDate.Subtract(TimeSpan.FromDays(30)).ToString("yyyy-MM-dd");
+            var expectedUri = new Uri($"https://api.github.com/search/repositories?q=created:>{monthBeforeToday}&sort=stars&order=desc&per_page=100");
+            var actualUri = RequestUriBuilder.BuildRequestUri();
+            Assert.AreEqual(expectedUri, actualUri);
+        }
+    }
+}
