83215ed79d2c82dec05083062917c5a68545f17b
youssef lghoumaigui <yousseflghoumaigui@gmail.com>
2021-06-07 02:28:53 +0100

    Introduce TrendingRepositoriesControllerTests

---
+++ backend-coding-challenge.Tests/Controllers/TrendingRepositoriesControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using backend_coding_challenge.Controllers;
+using backend_coding_challenge.GithubClient;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace backend_coding_challenge.Tests.Controllers
+{
+    internal class TrendingRepositoriesControllerTests
+    {
+        [Test]
+        public async Task GetTrendingRepositories_Returns_NotFound_In_Case_Of_Exception()
+        {
+            // Arrange
+            var trendingRepositoriesClient = new Mock<ITrendingRepositoriesClient>();
+            trendingRepositoriesClient.Setup(_ => _.GetTrendingRepositoriesAsync())
+                .Throws(new HttpRequestException("Failed to retrieve trending repositories from github api"));
+            var trendingRepositoriesController = new TrendingRepositoriesController(trendingRepositoriesClient.Object);
+
+            // Act
+            IActionResult trendingRepositories = await trendingRepositoriesController.GetTrendingRepositories();
+
+            // Assert
+            Assert.IsInstanceOf<NotFoundObjectResult>(trendingRepositories);
+        }
+    }
+}
